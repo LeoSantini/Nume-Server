8aa69829a76390875659afc115a17c36479ae41e
Leonardo Santini <leonardo.santini@outlook.com.br>
2022-05-13 13:16:18 -0300

    Bug fix user router

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -8,7 +8,7 @@ const User = require("../models/User.model"); // User
 
 const saltRounds = 10; // Salt rounds
 
-router.post("/crate-user", async (req, res) => {
+router.post("/create-user", async (req, res) => {
   try {
     const { password } = req.body; // Password
 
@@ -18,10 +18,12 @@ router.post("/crate-user", async (req, res) => {
         /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
       )
     ) {
-      return res.status(400).json({ msg: "Invalid password" }); // Invalid password
+      return res.status(400).json({
+        msg: "Invalid password. Password is required and must have at least 8 characters, uppercase and lowercase letters, numbers and special characters.",
+      }); // Invalid password
     }
-    const Salt = await bcrypt.genSalt(saltRounds); // Salt
-    const hashedPassword = await bcrypt.hash(password, Salt); // Hashed password
+    const salt = await bcrypt.genSalt(saltRounds); // Salt
+    const hashedPassword = await bcrypt.hash(password, salt); // Hashed password
 
     const newUser = await User.create({
       ...req.body,
@@ -29,10 +31,13 @@ router.post("/crate-user", async (req, res) => {
     }); // New user
 
     delete newUser._doc.passwordHash; // Delete password hash
+    delete newUser._doc.resetPasswordToken; // Delete reset password token
     delete newUser._doc.__v; // Delete version
 
+    console.log(newUser);
     return res.status(201).json(newUser._doc); // Created
   } catch (err) {
+    console.log(err);
     return res.status(500).json({ msg: "Internal server error:" + err.msg }); // Internal server error
   }
 });

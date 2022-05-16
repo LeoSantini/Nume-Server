daec3b87c20ecbd3c9ce832b0d8f9135d0bd9bbf
Leonardo Santini <leonardo.santini@outlook.com.br>
2022-05-16 13:24:17 -0300

    Create client post route

---
+++ routes/client.routes.js
@@ -0,0 +1,21 @@
+const express = require("express"); // Express
+const router = express.Router(); // Router
+const Client = require("../models/Client.model"); // Client
+const User = require("../models/User.model"); // User
+const isAuth = require("../middlewares/isAuth"); // Is auth
+const attachCurrentUser = require("../middlewares/attachCurrentUser"); // Attach current user
+
+router.post("/create-client", async (req, res) => {
+  // Create client
+  try {
+    const newClient = await Client.create(req.body);
+    console.log(newClient);
+    console.log(req.body);
+    return res.status(201).json(newClient);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "Internal server error:" + err.msg }); // Internal server error
+  }
+});
+
+module.exports = router; // Exports the router
